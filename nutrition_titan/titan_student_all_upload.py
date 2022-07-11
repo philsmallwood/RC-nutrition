7324d249a6e3b404c8568c671213ab88201295f5
Phil <philip.smallwood@redclay.k12.de.us>
2022-07-11 13:22:50 -0400

    Updating/refactoring Student Titan script

--- nutrition_titan/titan_student_all_upload.py
+++ nutrition_titan/titan_student_all_upload.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 ###RC Titan Student File Script
 ###Script to generate a student file with the necesary info for nturition
-###Requires time, pandas, keyring, and pysftp to be installed
+###Requires time, pandas, keyring, datetime, glob, and pysftp to be installed
 ###For keyring, need to set the username/password for sftp sites for downloads and uploads
 ###Includes Red Clay and Charter students
 
@@ -11,8 +11,8 @@ import keyring
 import pysftp
 import time
 import os
-import glob
 import sys
+from datetime import date
 #######
 
 ###Turns off the hashseed randomization###
@@ -31,22 +31,24 @@ dname = os.path.dirname(abspath)
 os.chdir(dname)
 #######
 
-###Set variables for script###
+#####Variables#####
 startTime = time.ctime()
 titanHostname = "sftp.titank12.com"
 titanUsername = "RCCSD"
-nutritionHostname ="10.222.2.70"
-nutritionUsername = "philip.smallwood"
 localStudentFilePath = '/uploads/DOE/student_master_primary-en.csv'
 localSecondaryStudentFilePath = '/uploads/DOE/student_master_secondary-en.csv'
+localCharterStudentFilePath = '/uploads/DSC/Follet/destinystudentsCharter.csv'
+localUrbanPromiseFilePath = '/RC-scripts/nutrition_titan/urbanpromisecurrent'
 localAllergyFilePath = '/uploads/DSC/Allergies/StudentAllergies.csv'
 localUpFilePath = './rc_titan_student.csv'
 remoteUpFilePath = '/rc_titan_student.csv'
 logFile = "/var/log/scripts/titan_student_upload.log"
-dropColumnsCharter = {4, 7, 9, 10, 11, 12, 15, 16, 17, 26, 27, 28, 29, 30, 31, 32}
-
+dropColumnsCharter = {1, 6, 10, 11, 13, 15, 16, 19, 20, 30, 31, 32, 33, 34, 35}
+earliestEnrollmentDate = '08/31/2022'
+today = date.today()
+todayStr = today.strftime("%m/%d/%Y")
 ###Set dictionary to rename columns
-###Secondary Dataframe
+##Secondary Dataframe
 colNames = {'Student ID': 'Student Id', 
             'First Name': 'Student First Name', 
             'Last Name': 'Student Last Name', 
@@ -66,27 +68,29 @@ colNames = {'Student ID': 'Student Id',
             'Home Phone Number': 'Home Phone', 
             'Guardian Work Phone Number': 'Work Phone', 
             'Email': 'Email - Guardian' }
-
-###Charter Dateframe
-colNamesCharter = { 0 : 'Student Id', 
-            2 : 'Student First Name', 
-            1 : 'Student Last Name', 
-            3 : 'Student Middle Name', 
-            5 : 'Current Building',
-            6 : 'Student Grade', 
-            14 : 'Federal Race Code', 
-            13 : 'Student Gender', 
-            18 : 'City - Physical',
-            19 : 'State - Physical', 
-            20 : 'Zip - Physical', 
-            8 : 'Student Homeroom Primary', 
-            23 : 'First Name - Guardian', 
-            22 : 'Last Name - Guardian',
-            24 : 'Middle Name - Guardian',
-            21 : 'Home Phone', 
-            25 : 'Work Phone'}
-#######
-###Charter Dateframe
+##Charter Dateframe
+colNamesCharter = { 0 : 'Current Building', 
+            2 : 'Student Id',
+            3 : 'Student Last Name',
+            4 : 'Student First Name', 
+            5 : 'Student Last Name', 
+            7 : 'Student Gender',
+            8 : 'Student Homeroom Primary',
+            9 : 'Student Grade', 
+            12 : 'Birthdate',
+            14 : 'Email - Guardian',
+            17 : 'Federal Race Code',
+            18 : 'Hispanic/Latino Ethnicity',
+            21 : 'City - Physical',
+            22 : 'State - Physical', 
+            23 : 'Zip - Physical', 
+            24 : 'First Name - Guardian', 
+            25 : 'Last Name - Guardian',
+            26 : 'Middle Name - Guardian',
+            27 : 'Home Phone', 
+            28 : 'Mobile Phone',
+            29 : 'Work Phone'}
+##Urban Promise Dateframe
 colNamesUrbanPromise = { 0 : 'Current Building', 
             1 : 'Student Id',
             2 : 'Student First Name',
@@ -98,66 +102,57 @@ colNamesUrbanPromise = { 0 : 'Current Building',
             8 : 'City - Physical',
             9 : 'State - Physical',
             10 : 'Zip - Physical'}
+############
 
-###Download Files###
-###Get Charter School Files
-###NOTE: need to get these streamlined into COGNOS
-with pysftp.Connection(host=nutritionHostname, username=nutritionUsername, password=keyring.get_password('NUTRITIONSERVER', nutritionUsername)) as sftp:
-    sftp.cwd('dailyenrollment')
-    files = sftp.listdir()
-    for file in files: 
-        if (file[-3:]=='txt' or file[-3:]=='xls'):
-            sftp.get(file)
-            sftp.remove(file)
-######
-
-###Read Files into Dataframes###
-##Read Titan Cognos report with Student Data to dataframe
+#####Read Files into Dataframes#####
+##Read Main Cognos report with Student Data to dataframe
 df_students = pd.read_csv(localStudentFilePath, encoding='cp1252', dtype=str)
-
 ##Read Secondary Cognos report with Student Data to dataframe
 df_students_other = pd.read_csv(localSecondaryStudentFilePath, encoding='cp1252', dtype=str)
-
-##Combine Charter school files into one file
-##Check for charter files and combine if exists
-if glob.glob('*.txt'):
-    os.system("cat *.txt >> chartercombined.csv")
-else:
-    os.system("python3 /usr/local/bin/mailsend.py 'philip.smallwood@redclay.k12.de.us' 'Script Could Not Run - No Charter Files' '/var/log/scripts/titan_student_upload.log' ")
-    exit()
-##Read new chartercombined file into a dataframe
-df_students_charters = pd.read_csv('chartercombined.csv', header=None, dtype=str)
-
-##Rename Urban Promise file to generic name if a new one is there
-if glob.glob('*.xls'):
-    files = os.listdir()
-    for file in files:
-        if (file[-3:]=='xls'):
-            os.rename(file,'urbanpromisecurrent')
-
+##Read Charter School file into dataframe
+df_students_charters = pd.read_csv(localCharterStudentFilePath, header=None, skiprows=1, dtype=str)
+##Read Urban Promise file into dataframe
+df_urbanpromise = pd.read_excel('urbanpromisecurrent', skiprows=1, header=None, dtype=str)
 ###Read Allergies file to dataframe
 df_allergies = pd.read_csv(localAllergyFilePath, dtype=str)
 ###Rename the StudentID field in allergies dataframe
 df_allergies.rename(columns={'StudentID':'Student Id'}, inplace=True)
-#######
+############
 
 ###Format Charter schools dataframe###
-###Create Birthdate column from month, day, year columns
-df_students_charters['Birthdate'] = df_students_charters[9].astype(str) + '/' + df_students_charters[10].astype(str) + '/20' + df_students_charters[11].astype(str)
 ###Combine Street Address Line 1 and Apartment to match other sources
-df_students_charters['Street Addr Line & Apt - Physical'] = df_students_charters[[16, 17]].apply(lambda x: ', '.join(x.dropna()), axis=1)
+df_students_charters['Street Addr Line & Apt - Physical'] = df_students_charters[[19, 20]].apply(lambda x: ', '.join(x.dropna()), axis=1)
 ###Rename the columns to match other sources for later merging
 df_students_charters.rename(columns=colNamesCharter, inplace=True)
 ###Drop columns that are not needed
 df_students_charters.drop(columns = dropColumnsCharter, inplace=True)
 ###Change Charter Race codes to match Federal Race codes
-df_students_charters.loc[df_students_charters['Federal Race Code'] == '6', ['Federal Race Code']] = '3' #Asian
-df_students_charters.loc[df_students_charters['Federal Race Code'] == '5', ['Federal Race Code']] = '6' #Caucasian
-df_students_charters.loc[df_students_charters['Federal Race Code'] == '2', ['Federal Race Code']] = '4' #Black/African
-df_students_charters.loc[df_students_charters['Federal Race Code'] == '1', ['Federal Race Code']] = '2' #American Indian/Alaskan
+df_students_charters.loc[df_students_charters['Federal Race Code'] == 'Asian', \
+    ['Federal Race Code']] = '3' #Asian
+df_students_charters.loc[df_students_charters['Federal Race Code'] == 'White', \
+    ['Federal Race Code']] = '6' #Caucasian
+df_students_charters.loc[df_students_charters['Federal Race Code'] == 'Black', \
+    ['Federal Race Code']] = '4' #Black/African
+df_students_charters.loc[df_students_charters['Federal Race Code'] == 'Native Am.', \
+    ['Federal Race Code']] = '2' #American Indian/Alaskan
+df_students_charters.loc[df_students_charters['Federal Race Code'] == 'Hawaiian', ['Federal Race Code']] = '5' #Native Hawaiian/Other Pacific Islander
+###Change Ethnicity to Y or N
+df_students_charters.loc[df_students_charters['Hispanic/Latino Ethnicity'] == 'Hispanic', ['Hispanic/Latino Ethnicity']] = 'Y' #Hispanic
+df_students_charters.loc[df_students_charters['Hispanic/Latino Ethnicity'] == 'Non-Hispanic', ['Hispanic/Latino Ethnicity']] = 'N' #Hispanic
+############
 
+###Format Urban Promise dataframe###
+df_urbanpromise[1].fillna('5544-' + df_urbanpromise[2] + df_urbanpromise[3], inplace=True)
+###Add leading zeros to teacherid to ensure 6 digits exactly
+df_urbanpromise[1] = df_urbanpromise[1].apply(lambda x: '{0:0>6}'.format(x))
+###Fix date format by making object a 'datetime' format and setting output
+df_urbanpromise[5] = pd.to_datetime(df_urbanpromise[5])
+df_urbanpromise[5] = df_urbanpromise[5].dt.strftime('%m/%d/%Y')
+###Rename columns for final output
+df_urbanpromise.rename(columns=colNamesUrbanPromise, inplace=True)
+############
 
-###Format Secondary file###
+###Format Secondary file dataframe###
 ###Rename the columns to match other sources for later merging
 df_students_other.rename(columns=colNames, inplace=True)
 ###Add '320' to the building code to match other sources
@@ -168,50 +163,53 @@ df_students_other['Street Addr Line & Apt - Physical'] = df_students_other[['Str
 df_students_other.drop(columns = ['Street Address Line 1', 'Street Address Line 1', 'Apartment'], inplace=True)
 ###Remove Students from Secondary file that are in the Main file
 df_students_noguard = (df_students_other[~df_students_other['Student Id'].isin(df_students['Student Id'])])
-########
+############
 
 ###Format Main Student dataframe###
 ###Swap Current and Alternate Building for special programs
-###NOTE: 07/14/2021
 ###Programs Affected - Meadowood and Early Years
 ##Meadowood
-df_students.loc[(df_students['Alternate Building Name'] == 'Forest Oak Elementary School') & \
+df_students.loc[(df_students['Alternate Building'] == '320240') & \
     (df_students['Current Building'] == '320516'), ['Current Building', 'Alternate Building']] = '320240', '320516'
-df_students.loc[(df_students['Alternate Building Name'] == 'H.B. duPont Middle School') & \
+df_students.loc[(df_students['Alternate Building'] == '320276') & \
     (df_students['Current Building'] == '320516'), ['Current Building', 'Alternate Building']] = '320276', '320516'
-df_students.loc[(df_students['Alternate Building Name'] == 'McKean High School') & \
+df_students.loc[(df_students['Alternate Building'] == '320294') & \
     (df_students['Current Building'] == '320516'), ['Current Building', 'Alternate Building']] = '320294', '320516'
 ##Early Years
-df_students.loc[(df_students['Alternate Building Name'] == 'Meadowood Program') & \
+df_students.loc[(df_students['Alternate Building'] == '320516') & \
     (df_students['Current Building'] == '320529'), ['Current Building', 'Alternate Building']] = '320516', '320529'
-df_students.loc[(df_students['Alternate Building Name'] == 'Mote Elementary School') & \
+df_students.loc[(df_students['Alternate Building'] == '320264') & \
     (df_students['Current Building'] == '320529'), ['Current Building', 'Alternate Building']] = '320264', '320529'
-df_students.loc[(df_students['Alternate Building Name'] == 'Richardson Park Learning Center') & \
+df_students.loc[(df_students['Alternate Building'] == '320254') & \
     (df_students['Current Building'] == '320529'), ['Current Building', 'Alternate Building']] = '320254', '320529'
-df_students.loc[(df_students['Alternate Building Name'] == 'Warner Elementary School') & \
+df_students.loc[(df_students['Alternate Building'] == '320266') & \
     (df_students['Current Building'] == '320529'), ['Current Building', 'Alternate Building']] = '320266', '320529'
-df_students.loc[(df_students['Alternate Building Name'] == 'Wm. C. Lewis Dual Language Elem.') & \
+df_students.loc[(df_students['Alternate Building'] == '320246') & \
     (df_students['Current Building'] == '320529'), ['Current Building', 'Alternate Building']] = '320246', '320529'
 ###Drop Z calendar (320888) and First State School (320530) students
 df_studentsno888 = df_students[df_students['Current Building'] != '320888']
 df_studentsno530or888 = df_studentsno888[df_studentsno888['Current Building'] != '320530']
-###Drop extra column at the end
-#df_final = df_no530or888.drop(columns = ['Alternate Building Name'])
-###Holding on dropping this until 09/01/2021
-df_studentsno530or888.loc[df_studentsno530or888['Alternate Building Name'] != 'a', ['Alternate Building Name']] = '8/31/2021'
-#########
+############
 
-###Add students from Secondary Student and Charter dataframes that are not in the Titan file###
+###Combine all of the Dataframes###
 df_students_combined = df_studentsno530or888.merge(df_students_noguard, how = 'outer')
 df_students_combined_charters = df_students_combined.merge(df_students_charters, how = 'outer')
-######
+df_allstudents = df_students_combined_charters(df_urbanpromise, how = 'outer')
+############
 
 ###Final Prep and Upload### 
 ###Add Allergies into main file
-df_studentallergies = df_students_combined_charters.merge(df_allergies[['Student Id', 'Allergies']], on = 'Student Id', how = 'left')
+df_allstudents_allergies = df_allstudents.merge(df_allergies[['Student Id', 'Allergies']], on = 'Student Id', how = 'left')
 
-###Reorder to final form
-df_final = df_studentallergies[['Student Id', 'Student First Name', 'Student Middle Name', 'Student Last Name', 'Student Generation', 'Allergies', 'Birthdate', 'Student Gender', 'Federal Race Code', 'Hispanic/Latino Ethnicity', 'Alternate Building', 'Current School Year', 'Current Building', 'Student Grade', 'Student Homeroom Primary', 'Street Addr Line & Apt - Physical', 'City - Physical', 'State - Physical', 'Zip - Physical', 'Street Addr Line & Apt - Mailing', 'City - Mailing', 'State - Mailing', 'Zip - Mailing', 'First Name - Guardian', 'Middle Name - Guardian', 'Last Name - Guardian', 'Mobile Phone', 'Home Phone', 'Work Phone', 'Email - Guardian', 'Relation Name - Guardian', 'Alternate Building Name']]
+###Reorder to Final Data Frame
+df_final = df_allstudents_allergies[['Student Id', 'Student First Name', 'Student Middle Name', \
+    'Student Last Name', 'Student Generation', 'Allergies', 'Birthdate', 'Student Gender', \
+    'Federal Race Code', 'Hispanic/Latino Ethnicity', 'Alternate Building', \
+    'Current School Year', 'Current Building', 'Student Grade', 'Student Homeroom Primary', \
+    'Street Addr Line & Apt - Physical', 'City - Physical', 'State - Physical', 'Zip - Physical', \
+    'Street Addr Line & Apt - Mailing', 'City - Mailing', 'State - Mailing', 'Zip - Mailing', \
+    'First Name - Guardian', 'Middle Name - Guardian', 'Last Name - Guardian', 'Mobile Phone', \
+    'Home Phone', 'Work Phone', 'Email - Guardian', 'Relation Name - Guardian']]
 
 ###Create Household ID based on Street Address
 df_final['HHID'] = df_final['Street Addr Line & Apt - Physical'].map(hash)
@@ -227,32 +225,20 @@ df_final['Zip - Mailing'].fillna(df_final['Zip - Physical'], inplace=True)
 ###Fill Guardian Relationship as Guardian if Blank
 df_final['Relation Name - Guardian'].fillna('Guardian', inplace=True)
 
-###Fill Alt Building if Blank
-df_final['Alternate Building Name'].fillna('8/31/2021', inplace=True)
+###Add Entry Date
+##Use the earliest Entry Date if it is before that date
+##Otherwise, use the current date
+if earliestEnrollmentDate > todayStr:
+    df_final['Enrollment Date'] = earliestEnrollmentDate
+else:
+    df_final['Enrollment Date'] = todayStr
 
-###Section for Urban Promise
-###We sporadically get files from Urban Promise
-###Adding logic to check before adding and exporting
-if os.path.exists('urbanpromisecurrent'):
-    df_urbanpromise = pd.read_excel('urbanpromisecurrent', skiprows=1, header=None, dtype=str)
-    df_urbanpromise[1].fillna('5544-' + df_urbanpromise[2] + df_urbanpromise[3], inplace=True)
-    ###Add leading zeros to teacherid to ensure 6 digits exactly
-    df_urbanpromise[1] = df_urbanpromise[1].apply(lambda x: '{0:0>6}'.format(x))
-    ###Fix date format by making object a 'datetime' format and setting output
-    df_urbanpromise[5] = pd.to_datetime(df_urbanpromise[5])
-    df_urbanpromise[5] = df_urbanpromise[5].dt.strftime('%m/%d/%Y')
-    ###Rename columns for final output
-    df_urbanpromise.rename(columns=colNamesUrbanPromise, inplace=True)
-    ###Create final dataframe and output
-    df_allstudents = df_final.merge(df_urbanpromise, how = 'outer')
-    df_allstudents.to_csv(localUpFilePath, index=False)
-else:    
-###Export to data to csv file if no urban promise file
+###Export to data to csv file
     df_final.to_csv(localUpFilePath, index=False)
 
 ###Upload file to Titan
-with pysftp.Connection(host=titanHostname, username=titanUsername, password=keyring.get_password("TITANK12", "RCCSD")) as sftp:
-    sftp.put(localUpFilePath, remoteUpFilePath)
+#with pysftp.Connection(host=titanHostname, username=titanUsername, password=keyring.get_password("TITANK12", "RCCSD")) as sftp:
+#    sftp.put(localUpFilePath, remoteUpFilePath)
 
 
 ###Logging
@@ -268,9 +254,5 @@ os.system("python3 /usr/local/bin/mailsend.py 'philip.smallwood@redclay.k12.de.u
 ###Move Uploaded File to archive
 os.rename(localUpFilePath,time.strftime("/archive/%Y%m%d%H%M%S-TitanStudentFile.csv"))
 
-###Remove downloaded files
-fileToDelete = os.listdir()
-for file in fileToDelete:
-    if (file[-3:]=='txt') or (file[-3:]=='csv') or (file[-4:]=='xlsx'):
-        os.remove(file)
+
 ########
