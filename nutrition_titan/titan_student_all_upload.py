63f08d0f7e161addd2158d0cc47427ca12edb75b
Phil <philmore1979@gmail.com>
2022-10-26 08:50:35 -0400

    Added Language to file and cleaned up

--- nutrition_titan/titan_student_all_upload.py
+++ nutrition_titan/titan_student_all_upload.py
@@ -35,6 +35,7 @@ localStudentFilePath = '/uploads/DOE/StudentAccountCreationFile.csv'
 localCharterStudentFilePath = '/uploads/DSC/Follet/destinystudentsCharter.csv'
 localUrbanPromiseFilePath = '/uploads/nutrition/urbanpromise/urbanpromisecurrent'
 localAllergyFilePath = '/uploads/DSC/Allergies/StudentAllergies.csv'
+localStudentLanguageFilePath = '/uploads/DOE/student_home_lang-en.csv'
 localUpFilePath = '/RC-scripts/nutrition_titan/rc_titan_student.csv'
 remoteUpFilePath = '/rc_titan_student.csv'
 logFile = "/var/log/scripts/titan_student_upload.log"
@@ -47,7 +48,8 @@ logToEmail = 'philip.smallwood@redclay.k12.de.us'
 logSubject = 'Titan Student File Uploaded'
 ###Set dictionary to rename columns
 ##Charter Dateframe
-colNamesCharter = { 0 : 'Current Building', 
+colNamesCharter = { 
+            0 : 'Current Building',
             2 : 'Student Id',
             3 : 'Student Last Name',
             4 : 'Student First Name', 
@@ -81,87 +83,112 @@ colNamesUrbanPromise = {
             8 : 'City - Physical',
             9 : 'State - Physical',
             10 : 'Zip - Physical'}
+##Allergies Dataframe
+colNamesAllergies = {
+    'StudentID':'Student Id'
+}
+##Language Dataframe
+colNamesLanguage = {
+    0 : 'Student Id', 
+    1 : 'Student Language'
+}
 ############
 
 #####Read Files into Dataframes#####
-##Read Student File with Student Data to dataframe
-df_students = pd.read_csv(localStudentFilePath, dtype=str)
-df_students = df_stripper(df_students)
-##Read Charter School file into dataframe
-df_students_charters = pd.read_csv(localCharterStudentFilePath, header=None, skiprows=1, dtype=str, on_bad_lines='skip')
-df_students_charters = df_stripper(df_students_charters)
-##Read Urban Promise file into dataframe
-df_urbanpromise = pd.read_excel(localUrbanPromiseFilePath, skiprows=1, header=None, dtype=str)
-df_urbanpromise = df_stripper(df_urbanpromise)
-###Read Allergies file to dataframe
-df_allergies = pd.read_csv(localAllergyFilePath, dtype=str)
-###Rename the StudentID field in allergies dataframe
-df_allergies.rename(columns={'StudentID':'Student Id'}, inplace=True)
+##Read RC Student File to Dataframe
+df_RCstudents = pd.read_csv(localStudentFilePath, dtype=str)
+df_RCstudents = df_stripper(df_RCstudents)
+##Read Charter School File to Dataframe
+df_CharterStudents = pd.read_csv(localCharterStudentFilePath, header=None, skiprows=1, dtype=str, on_bad_lines='skip')
+df_CharterStudents = df_stripper(df_CharterStudents)
+##Read Urban Promise File to Dataframe
+#Format Can Be Excel or CSV
+try:
+    df_UrbanPromise = pd.read_excel(localUrbanPromiseFilePath, skiprows=1, header=None, dtype=str)
+    df_UrbanPromise = df_stripper(df_UrbanPromise)
+except:
+    df_UrbanPromise = pd.read_csv(localUrbanPromiseFilePath, skiprows=1, header=None, dtype=str)
+    df_UrbanPromise = df_stripper(df_UrbanPromise)
+###Read Allergies File to Dataframe
+df_Allergies = pd.read_csv(localAllergyFilePath, dtype=str)
+#Rename the StudentID Field in Allergies DataFrame
+df_Allergies.rename(columns=colNamesAllergies, inplace=True)
+###Read Language File to Dataframe
+df_Languages = pd.read_csv(localStudentLanguageFilePath, dtype=str, header=None, skiprows=1 )
+#Rename the StudentID Field in Languages DataFrame
+df_Languages.rename(columns=colNamesLanguage, inplace=True)
 ############
 
 ###Format Charter schools dataframe###
 ###Combine Street Address Line 1 and Apartment to match other sources
-df_students_charters['Street Addr Line & Apt - Physical'] = df_students_charters[[19, 20]].apply(lambda x: ', '.join(x.dropna()), axis=1)
+df_CharterStudents['Street Addr Line & Apt - Physical'] = df_CharterStudents[[19, 20]].apply(lambda x: ', '.join(x.dropna()), axis=1)
 ###Rename the columns to match other sources for later merging
-df_students_charters.rename(columns=colNamesCharter, inplace=True)
+df_CharterStudents.rename(columns=colNamesCharter, inplace=True)
 ###Drop columns that are not needed
-df_students_charters.drop(columns = dropColumnsCharter, inplace=True)
+df_CharterStudents.drop(columns = dropColumnsCharter, inplace=True)
 ###Change Charter Race codes to match Federal Race codes
-df_students_charters.loc[df_students_charters['Federal Race Code'] == 'Asian', \
+df_CharterStudents.loc[df_CharterStudents['Federal Race Code'] == 'Asian', \
     ['Federal Race Code']] = '3' #Asian
-df_students_charters.loc[df_students_charters['Federal Race Code'] == 'White', \
+df_CharterStudents.loc[df_CharterStudents['Federal Race Code'] == 'White', \
     ['Federal Race Code']] = '6' #Caucasian
-df_students_charters.loc[df_students_charters['Federal Race Code'] == 'Black', \
+df_CharterStudents.loc[df_CharterStudents['Federal Race Code'] == 'Black', \
     ['Federal Race Code']] = '4' #Black/African
-df_students_charters.loc[df_students_charters['Federal Race Code'] == 'Native Am.', \
+df_CharterStudents.loc[df_CharterStudents['Federal Race Code'] == 'Native Am.', \
     ['Federal Race Code']] = '2' #American Indian/Alaskan
-df_students_charters.loc[df_students_charters['Federal Race Code'] == 'Hawaiian', ['Federal Race Code']] = '5' #Native Hawaiian/Other Pacific Islander
+df_CharterStudents.loc[df_CharterStudents['Federal Race Code'] == 'Hawaiian', \
+    ['Federal Race Code']] = '5' #Native Hawaiian/Other Pacific Islander
 ###Change Ethnicity to Y or N
-df_students_charters.loc[df_students_charters['Hispanic/Latino Ethnicity'] == 'Hispanic', ['Hispanic/Latino Ethnicity']] = 'Y' #Hispanic
-df_students_charters.loc[df_students_charters['Hispanic/Latino Ethnicity'] == 'Non-Hispanic', ['Hispanic/Latino Ethnicity']] = 'N' #Hispanic
+df_CharterStudents.loc[df_CharterStudents['Hispanic/Latino Ethnicity'] == 'Hispanic', \
+    ['Hispanic/Latino Ethnicity']] = 'Y' #Hispanic
+df_CharterStudents.loc[df_CharterStudents['Hispanic/Latino Ethnicity'] == 'Non-Hispanic', \
+    ['Hispanic/Latino Ethnicity']] = 'N' #Hispanic
 ############
 
 ###Format Urban Promise dataframe###
-df_urbanpromise = df_urbanpromise.loc[df_urbanpromise[0] == '5544']
+df_UrbanPromise = df_UrbanPromise.loc[df_UrbanPromise[0] == '5544']
 ###Fix date format by making object a 'datetime' format and setting output
-df_urbanpromise[5] = pd.to_datetime(df_urbanpromise[5])
-df_urbanpromise[5] = df_urbanpromise[5].dt.strftime('%m/%d/%Y')
+df_UrbanPromise[5] = pd.to_datetime(df_UrbanPromise[5])
+df_UrbanPromise[5] = df_UrbanPromise[5].dt.strftime('%m/%d/%Y')
 ###Add leading zeros to studentID to ensure 6 digits exactly
-df_urbanpromise[1] = df_urbanpromise[1].str.zfill(6)
+df_UrbanPromise[1] = df_UrbanPromise[1].str.zfill(6)
 ###Add leading zeros to Grade to ensure 2 digits exactly
-df_urbanpromise[4] = df_urbanpromise[4].str.zfill(2)
+df_UrbanPromise[4] = df_UrbanPromise[4].str.zfill(2)
 ###Fix Kindergarten Grade
-df_urbanpromise[4] = df_urbanpromise[4].str.replace('0K','KN')
+df_UrbanPromise[4] = df_UrbanPromise[4].str.replace('0K','KN')
 ###Rename columns for final output
-df_urbanpromise.rename(columns=colNamesUrbanPromise, inplace=True)
+df_UrbanPromise.rename(columns=colNamesUrbanPromise, inplace=True)
 ############
 
 ###Format Main Student dataframe###
 ###Drop Z calendar (320888) and First State School (320530) students
-df_studentsno888 = df_students[df_students['Current Building'] != '888']
-df_studentsno530or888 = df_studentsno888[df_studentsno888['Current Building'] != '530']
-df_studentsno530or888['Current Building'] = '320' + df_studentsno530or888['Current Building']
+df_RCstudentsno530or888 = df_RCstudents[ (df_RCstudents['Current Building'] != '888') & \
+    (df_RCstudents['Current Building'] != '530') ]
+df_RCstudentsno530or888['Student Building'] = '320' + df_RCstudentsno530or888['Current Building']
 ############
 
 ###Combine all of the Dataframes###
-##Add allergies for RC students to Main
-df_students_combined_allergies = df_studentsno530or888.merge(df_allergies[['Student Id', 'Allergies']], on = 'Student Id', how = 'left')
+##Add allergies for RC Students to Main
+df_RCStudents_Allergies = df_RCstudentsno530or888.merge(df_Allergies[['Student Id', 'Allergies']], \
+    on = 'Student Id', how = 'left')
+##Add Languages for RC Students to Main
+df_RCStudents_Allergies_Lang = df_RCStudents_Allergies.merge(df_Languages[['Student Id', 'Student Language']], \
+    on = 'Student Id', how = 'left')
 ##Add Charter Students to Main
-df_students_combined_charters = df_students_combined_allergies.merge(df_students_charters, how = 'outer')
+df_RCstudents_Charters = df_RCStudents_Allergies_Lang.merge(df_CharterStudents, how = 'outer')
 ##Add Urban Promise Students to Main
-df_allstudents = df_students_combined_charters.merge(df_urbanpromise, how = 'outer')
+df_AllStudents = df_RCstudents_Charters.merge(df_UrbanPromise, how = 'outer')
 ############
 
 ###Final Prep and Upload### 
 ###Reorder to Final Data Frame
-df_final = df_allstudents[['Student Id', 'Student First Name', 'Student Middle Name', \
+df_final = df_AllStudents[['Student Id', 'Student First Name', 'Student Middle Name', \
     'Student Last Name', 'Student Generation', 'Allergies', 'Birthdate', 'Student Gender', \
     'Federal Race Code', 'Hispanic/Latino Ethnicity', 'Alternate Building', \
-    'Current School Year', 'Current Building', 'Student Grade', 'Student Homeroom Primary', \
+    'Current School Year', 'Student Building', 'Student Grade', 'Student Homeroom Primary', \
     'Street Addr Line & Apt - Physical', 'City - Physical', 'State - Physical', 'Zip - Physical', \
     'Street Addr Line & Apt - Mailing', 'City - Mailing', 'State - Mailing', 'Zip - Mailing', \
     'First Name - Guardian', 'Middle Name - Guardian', 'Last Name - Guardian', 'Mobile Phone', \
-    'Home Phone', 'Work Phone', 'Email - Guardian', 'Relation Name - Guardian']]
+    'Home Phone', 'Work Phone', 'Email - Guardian', 'Relation Name - Guardian', 'Student Language']]
 
 ###Create Household ID based on Street Address
 df_final['HHID'] = df_final['Street Addr Line & Apt - Physical'].map(hash)
@@ -189,7 +216,8 @@ else:
 df_final.to_csv(localUpFilePath, index=False)
 
 ###Upload file to Titan
-with pysftp.Connection(host=titanHostname, username=titanUsername, password=keyring.get_password("TITANK12", "RCCSD")) as sftp:
+with pysftp.Connection(host=titanHostname, username=titanUsername, \
+    password=keyring.get_password("TITANK12", "RCCSD")) as sftp:
     sftp.put(localUpFilePath, remoteUpFilePath)
 
 
@@ -206,6 +234,4 @@ mail_send(logToEmail,logSubject,logFile)
 
 ###Move Uploaded File to archive
 os.rename(localUpFilePath,time.strftime("/archive/%Y%m%d%H%M%S-TitanStudentFile.csv"))
-
-
 ########
