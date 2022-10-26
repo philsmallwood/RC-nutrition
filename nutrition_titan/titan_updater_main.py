ea3af93341169aa8934960bd66b4544b88a08054
Phil <philmore1979@gmail.com>
2022-10-26 15:13:04 -0400

    Continuing work

--- nutrition_titan/titan_updater_main.py
+++ nutrition_titan/titan_updater_main.py
@@ -6,8 +6,8 @@
 ### Scripts: 
 # titan_urbanpromise_file_download.py
 # titan_student_all_upload.py
-# classlink_dsc_file_fix.py
-# classlink_upload.py
+# titan_dircert_prep.py
+# titan_staff_upload.py
 
 ###Import Modules###
 import time
@@ -22,15 +22,16 @@ CurrentDate = datetime.date.today()
 Date = CurrentDate.strftime('%m-%d-%Y')
 startTime = time.ctime()
 #Scripts
-scriptPath = '/RC-scripts/classlink/classlink_updater/'
+scriptPath = '/RC-scripts/nutrition_titan/'
 urbanPromiseFileScript = 'titan_urbanpromise_file_download.py'
 studentFileScript = 'titan_student_all_upload.py'
-directCertScript = 'classlink_dsc_file_fix.py'
+directCertScript = 'titan_dircert_prep.py'
+staffFileScript = 'titan_staff_upload.py'
 uploadscript = 'classlink_upload.py'
 #Email Vars
 logToEmail = 'philip.smallwood@redclay.k12.de.us'
-logSubject = 'Classlink Updates Log'
-logFile = "/var/log/scripts/Classlink-" + Date +".log"
+logSubject = 'Titan Updates Log'
+logFile = "/var/log/scripts/Titan-" + Date +".log"
 ##Function Definitions
 #Function to call a python script and append info to a log file
 def pyscript_call(scriptPath,scriptName,logFile):
@@ -42,6 +43,14 @@ def pyscript_call(scriptPath,scriptName,logFile):
     f.write("The " + scriptName + " script ran successfully \n")
     f.write("---\n")
     f.close()
+def log_script_error(scriptName,logFile)
+    #Write the Error to the Log
+        ###Log Error
+    f = open(logFile, "a")
+    f.write("---\n")
+    f.write("The " + scriptName + " failed! \n")
+    f.write("---\n")
+    f.close()
 ###########
 
 ###Log Begin
@@ -51,23 +60,42 @@ f.write("The Titan Updater Script was started on " + startTime + "\n")
 f.write("---\n")
 f.close()
 
-###Admin File Generator###
+###Urban Promise File Downloader###
 try:
-    pyscript_call(scriptPath,adminScript,logFile)
+    pyscript_call(scriptPath,urbanPromiseFileScript,logFile)
+except:
+    log_script_error(urbanPromiseFileScript,logFile)
+
+###Student Updater File Generator###
+try:
+    pyscript_call(scriptPath,studentFileScript,logFile)
 except:
     ###Log Error
     f = open(logFile, "a")
     f.write("---\n")
-    f.write("The " + adminScript + " failed! \n")
+    f.write("The " + studentFileScript + " failed! \n")
     f.write("---\n")
     f.close()
 
+###Direct Certification File Generator###
+try:
+    pyscript_call(scriptPath,directCertScript,logFile)
+except:
+    ###Log Error
+    f = open(logFile, "a")
+    f.write("---\n")
+    f.write("The " + directCertScript + " failed! \n")
+    f.write("---\n")
+    f.close()
 
-#Call Urban Promise Downloader
-
-#Call Student Updater
-
-## call Direct Cert 
-
-##Call Staff
+###Staff Updater  File Generator###
+try:
+    pyscript_call(scriptPath,staffFileScript,logFile)
+except:
+    ###Log Error
+    f = open(logFile, "a")
+    f.write("---\n")
+    f.write("The " + staffFileScript + " failed! \n")
+    f.write("---\n")
+    f.close()
 
