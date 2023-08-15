883f24ee30f6e5c11908a299fa075d321fba84cd
Phil <phil.smallwood@live.com>
2023-08-15 17:22:39 -0400

    reworked urban promise

--- nutrition_titan_updater/titan_student_file_generator.py
+++ nutrition_titan_updater/titan_student_file_generator.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 ###RC Titan Student File Generator Script
-###Script to generate a student file with the necesary info for nturition
+###Script to generate a student file with the necesary info for nutrition
 ###Includes Red Clay and Charter students
 
 def titan_student_file_generator():
@@ -10,6 +10,7 @@ def titan_student_file_generator():
     from os import getenv
     from datetime import date
     from dotenv import load_dotenv
+    from titan_urban_promise_data_download import titan_urban_promis_data_download
     from dfcleanup import df_stripper #Self Created Module
     #######
     
@@ -20,11 +21,10 @@ def titan_student_file_generator():
     #Date
     current_date = date.today()
     student_date = current_date.strftime('%m/%d/%Y')
-    earliest_student_start_date = '08/29/2023'
+    earliest_student_start_date = '08/28/2023'
     #File Locations
     student_file_path = getenv('student_file_path')
     charter_student_file_path = getenv('charter_student_file_path')
-    urban_promise_file_path = getenv('urban_promise_file_path')
     allergy_file_path = getenv('allergy_file_path')
     student_language_file_path = getenv('student_language_file_path')
     titan_student_final_file = getenv('titan_student_final_file')
@@ -56,19 +56,6 @@ def titan_student_file_generator():
                 27 : 'Home Phone', 
                 28 : 'Mobile Phone',
                 29 : 'Work Phone'}
-    #Urban Promise Dateframe
-    col_names_urban_promise = { 
-                0 : 'Student Building',
-                1 : 'Student Id',
-                2 : 'Student First Name',
-                3 : 'Student Last Name',
-                4 : 'Student Grade',
-                5 : 'Birthdate',
-                6 : 'Allergies',
-                7 : 'Street Addr Line & Apt - Physical',
-                8 : 'City - Physical',
-                9 : 'State - Physical',
-                10 : 'Zip - Physical'}
     #Allergies Dataframe
     col_names_allergies = {
         'StudentID':'Student Id'
@@ -88,15 +75,8 @@ def titan_student_file_generator():
         header=None, skiprows=1, dtype=str, on_bad_lines='skip')
     df_charter_students = df_stripper(df_charter_students)
     #Read Urban Promise File to Dataframe
-    #Format Can Be Excel or CSV
-    try:
-        df_urban_promise_students = pd.read_excel(urban_promise_file_path, \
-            skiprows=1, header=None, dtype=str)
-        df_urban_promise_students = df_stripper(df_urban_promise_students)
-    except:
-        df_urban_promise_students = pd.read_csv(urban_promise_file_path, \
-            skiprows=1, header=None, dtype=str)
-        df_urban_promise_students = df_stripper(df_urban_promise_students)
+    urban_promise_download = titan_urban_promis_data_download()
+    df_urban_promise_students = urban_promise_download[0]
     #Read Allergies File to Dataframe
     df_allergies = pd.read_csv(allergy_file_path, dtype=str)
     #Rename the StudentID Field in Allergies DataFrame
@@ -127,24 +107,10 @@ def titan_student_file_generator():
     df_charter_students.loc[df_charter_students['Federal Race Code'] == 'Hawaiian', \
         ['Federal Race Code']] = '5' #Native Hawaiian/Other Pacific Islander
     ###Change Ethnicity to Y or N
-    df_charter_students.loc[df_charter_students['Hispanic/Latino Ethnicity'] == 'Hispanic', \
-        ['Hispanic/Latino Ethnicity']] = 'Y' #Hispanic
-    df_charter_students.loc[df_charter_students['Hispanic/Latino Ethnicity'] == 'Non-Hispanic', \
-        ['Hispanic/Latino Ethnicity']] = 'N' #Non-Hispanic
-    ############
-    ###Format Urban Promise dataframe###
-    df_urban_promise_students = df_urban_promise_students.loc[df_urban_promise_students[0] == '5544']
-    ###Fix date format by making object a 'datetime' format and setting output
-    df_urban_promise_students[5] = pd.to_datetime(df_urban_promise_students[5])
-    df_urban_promise_students[5] = df_urban_promise_students[5].dt.strftime('%m/%d/%Y')
-    ###Add leading zeros to studentID to ensure 6 digits exactly
-    df_urban_promise_students[1] = df_urban_promise_students[1].str.zfill(6)
-    ###Add leading zeros to Grade to ensure 2 digits exactly
-    df_urban_promise_students[4] = df_urban_promise_students[4].str.zfill(2)
-    ###Fix Kindergarten Grade
-    df_urban_promise_students[4] = df_urban_promise_students[4].str.replace('0K','KN')
-    ###Rename columns for final output
-    df_urban_promise_students.rename(columns=col_names_urban_promise, inplace=True)
+    df_charter_students.loc[df_charter_students['Hispanic/Latino Ethnicity'] == \
+        'Hispanic', ['Hispanic/Latino Ethnicity']] = 'Y' #Hispanic
+    df_charter_students.loc[df_charter_students['Hispanic/Latino Ethnicity'] == \
+        'Non-Hispanic', ['Hispanic/Latino Ethnicity']] = 'N' #Non-Hispanic
     ############
     ###Format Main Student dataframe###
     ###Drop Z calendar (320888) and First State School (320530) students
@@ -157,25 +123,31 @@ def titan_student_file_generator():
     df_rc_students = df_rc_students.merge(df_allergies[['Student Id', 'Allergies']], \
         on = 'Student Id', how = 'left')
     ##Add Languages for RC Students to Main
-    df_rc_students = df_rc_students.merge(df_languages[['Student Id', 'Student Language']], \
-        on = 'Student Id', how = 'left')
+    df_rc_students = df_rc_students.merge(df_languages[['Student Id', \
+        'Student Language']], on = 'Student Id', how = 'left')
     ##Add Charter Students to Main
-    df_rc_and_charter_students = df_rc_students.merge(df_charter_students, how = 'outer')
+    df_rc_and_charter_students = df_rc_students.merge(df_charter_students, \
+        how = 'outer')
     ##Add Urban Promise Students to Main
-    df_all_students = df_rc_and_charter_students.merge(df_urban_promise_students, how = 'outer')
+    df_all_students = df_rc_and_charter_students.merge(df_urban_promise_students, \
+        how = 'outer')
     ############
     ###Final Prep and Upload### 
     ###Reorder to Final Data Frame
-    df_final = df_all_students[['Student Id', 'Student First Name', 'Student Middle Name', \
-        'Student Last Name', 'Student Generation', 'Allergies', 'Birthdate', 'Student Gender', \
-        'Federal Race Code', 'Hispanic/Latino Ethnicity', 'Alternate Building', \
-        'Current School Year', 'Student Building', 'Student Grade', 'Student Homeroom Primary', \
-        'Street Addr Line & Apt - Physical', 'City - Physical', 'State - Physical', 'Zip - Physical', \
-        'Street Addr Line & Apt - Mailing', 'City - Mailing', 'State - Mailing', 'Zip - Mailing', \
-        'First Name - Guardian', 'Middle Name - Guardian', 'Last Name - Guardian', 'Mobile Phone', \
-        'Home Phone', 'Work Phone', 'Email - Guardian', 'Relation Name - Guardian', 'Student Language']].copy()
+    df_final = df_all_students[['Student Id', 'Student First Name', \
+        'Student Middle Name', 'Student Last Name', 'Student Generation', \
+        'Allergies', 'Birthdate', 'Student Gender', 'Federal Race Code', \
+        'Hispanic/Latino Ethnicity', 'Alternate Building', 'Current School Year', \
+        'Student Building', 'Student Grade', 'Student Homeroom Primary', \
+        'Street Addr Line & Apt - Physical', 'City - Physical', \
+        'State - Physical', 'Zip - Physical', 'Street Addr Line & Apt - Mailing', \
+        'City - Mailing', 'State - Mailing', 'Zip - Mailing', 'First Name - Guardian', \
+        'Middle Name - Guardian', 'Last Name - Guardian', 'Mobile Phone', \
+        'Home Phone', 'Work Phone', 'Email - Guardian', 'Relation Name - Guardian', \
+        'Student Language']].copy()
     ###Create Household ID Based on Street Address Using Hashlib.md5
-    df_final['HHID'] = df_final['Street Addr Line & Apt - Physical'].apply(lambda x: md5(x.encode()).hexdigest())
+    df_final['HHID'] = df_final['Street Addr Line & Apt - Physical'].\
+        apply(lambda x: md5(x.encode()).hexdigest())
     ###Make HouseHold ID shorter
     df_final['HHID'] = df_final['HHID'].astype(str).str[1:16]
     ###Copy Physical Address to Mailing Address if Blank
