53199401b1b5e8db76b3465ab45e91dcf96bf4f3
Phil <phil.smallwood@live.com>
2023-08-15 18:00:10 -0400

    wrong df

--- nutrition_titan_updater/titan_dircert_file_generator.py
+++ nutrition_titan_updater/titan_dircert_file_generator.py
@@ -3,6 +3,7 @@ def titan_dircert_file_generator():
     import pandas as pd
     from io import StringIO
     from os import getenv
+    
     from dotenv import load_dotenv
     from pydrive2.drive import GoogleDrive
     from rc_google_py import login_google_service_account
@@ -74,7 +75,7 @@ def titan_dircert_file_generator():
     df_final['StudentID'] = df_dircerts['StudentID1'].astype(str).str.zfill(6)
     df_final['Type'] = df_dircerts['Type']
     df_final['Entry_Date'] = pd.to_datetime(df_dircerts['Entry_Date'])
-    df_final['Entry_Date'] = df_dircerts['Entry_Date'].dt.strftime('%m/%d/%Y')
+    df_final['Entry_Date'] = df_final['Entry_Date'].dt.strftime('%m/%d/%Y')
     #Export Final DataFrame to File
     df_final.to_csv(direct_cert_file_path, index=False)
     #Return Message
