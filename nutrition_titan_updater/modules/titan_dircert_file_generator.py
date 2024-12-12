8eedeccd6dcd31df79269ab04045033d67f61a0f
Phil <phil.smallwood@live.com>
2024-12-12 14:01:13 -0500

    added option to stop LowMemory warning

--- nutrition_titan_updater/modules/titan_dircert_file_generator.py
+++ nutrition_titan_updater/modules/titan_dircert_file_generator.py
@@ -1,25 +1,18 @@
-def titan_dircert_file_generator():
+def titan_dircert_file_generator(google_auth_key, 
+                                nutrition_shared_drive_id,
+                                direct_cert_shared_folder_id,
+                                direct_cert_file_path):
+    
     ### Import Modules ###
     import pandas as pd
     from io import StringIO
-    from os import getenv
-    from dotenv import load_dotenv
     from pydrive2.drive import GoogleDrive
     from rc_google_py import login_google_service_account
     #######
 
-    ### Variable s###
-    # Load .ENV File
-    load_dotenv()
-    # File Vars
-    direct_cert_file_path = getenv('direct_cert_file_path')
-    # Google Vars
-    google_auth_key = getenv('google_auth_key')
-    nutrition_shared_drive_id = getenv('nutrition_shared_drive_id')
-    direct_cert_shared_folder_id = getenv('direct_cert_shared_folder_id')
-    google_drive_query = f"{direct_cert_shared_folder_id} in parents and trashed=false"
+    ### Variables ###
+    google_drive_query = f"'{direct_cert_shared_folder_id}' in parents and trashed=false"
     # Empty Vars
-    log_entry = str()
     dircert_file_ids = []
     df_dircerts = pd.DataFrame()
     df_final = pd.DataFrame()
@@ -42,29 +35,17 @@ def titan_dircert_file_generator():
         if len(dircert_file_list) > 0:
             # Get Direct Cert File IDs
             for file in dircert_file_list:
-                #Only Grab Files, Not Folders
+                # Only Grab Files, Not Folders
                 if file['mimeType'] != 'application/vnd.google-apps.folder':
                     dircert_file_ids.append(file['id'])
             # Extract Direct Cert Data to DataFrame
             for id in dircert_file_ids:
                 file_instance = drive.CreateFile({'id':id})
                 file_info = file_instance.GetContentString()
-                df_temp = pd.read_csv(StringIO(file_info), skiprows=3)
+                df_temp = pd.read_csv(StringIO(file_info), skiprows=3, dtype=str)
                 df_dircerts = pd.concat([df_temp,df_dircerts])
-            # Log Entry
-            log_entry += "------------------\n"
-            log_entry += "Direct Cert Data Downloaded \n"
-            log_entry += "------------------\n"
-        else:
-            #Log Entry
-            log_entry += "------------------\n"
-            log_entry += "No Direct Cert Files to Process \n"
-            log_entry += "------------------\n"
     except:
-        #Error Log Entry
-        log_entry += "------------------\n"
-        log_entry += "!! Error !! - Problem Downloading Direct Cert Data \n"
-        log_entry += "------------------\n"
+        pass
     #######
 
     ### Determine Benefit Type ###
@@ -83,8 +64,6 @@ def titan_dircert_file_generator():
     # Export Final DataFrame to File
     df_final.to_csv(direct_cert_file_path, index=False)
     # Return Message
-    return log_entry
 
-if __name__ == '__main__':
-    log_entry = titan_dircert_file_generator()
-    print(log_entry)
+    return df_final
+    #return f"Direct Cert File Generated: {direct_cert_file_path}"
